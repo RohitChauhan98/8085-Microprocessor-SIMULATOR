2fa68ef5efdffbba08b6af8ce2792c6f841d255c
RohitChauhan98 <rc56951@gmail.com>
2023-05-10 19:13:25 +0530

    addressing done

--- arithmetic.cpp
+++ arithmetic.cpp
@@ -2,24 +2,50 @@
 #include "compiler.h"
 
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-void Arithmetic::ADD(string address, int *register, int *A)
+void Arithmetic::toBinary(int num, vector<int> &str)
+{
+    while (num != 0)
+    {
+        if (num % 2 == 0)
+        {
+            str.push_back('0');
+            num = num / 2;
+        }
+        else
+        {
+            str.push_back('1');
+            num = num / 2;
+        }
+    }
+    reverse(str.begin(), str.end());
+}
+
+void Arithmetic::flagStatus(int num1, int num2)
+{
+    vector<int> temp1 , temp2;
+    toBinary(num1, temp1);
+    toBinary(num2, temp2);
+}
+
+void Arithmetic::ADD(string address, int *regis, int *A)
 {
     char temp = address[0];
     switch (temp)
     {
     case 'A':
-        *A = *A + *register;
+        *A = *A + *regis;
         break;
     case 'B':
-        *A = *A + *register;
+        *A = *A + *regis;
         break;
     case 'C':
-        *A = *A + *register;
+        *A = *A + *regis;
         break;
     case 'D':
-        *A = *A + *register;
+        *A = *A + *regis;
         break;
 
     default:
@@ -27,6 +53,6 @@ void Arithmetic::ADD(string address, int *register, int *A)
     }
 }
 
-void Arithmetic::SUB(string address, int *register, int A)
+void Arithmetic::SUB(string address, int *regis, int *A)
 {
 }
--- arithmetic.h
+++ arithmetic.h
@@ -1,14 +1,16 @@
 #ifndef ARITHMETIC_H
 #define ARITHMETIC_H
 #include <string>
-
+#include<vector>
 using namespace std;
 
 class Arithmetic
 {
 public:
-    void ADD(std::string address, int *register, int A);
-    void SUB(std::string address, int *register, int A);
+    void ADD(std::string address, int *regis, int *A);
+    void SUB(std::string address, int *regis, int *A);
+    void toBinary(int num,std::vector<int> &str);
+    void flagStatus(int num1, int num2);
 };
 
 #endif
